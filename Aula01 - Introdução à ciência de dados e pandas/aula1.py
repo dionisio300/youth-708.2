8ec5ce38d18d12f538c594f6572241a0a3a56cb3
dionisio300 <dionisioassis80@gmail.com>
2025-05-17 09:51:31 -0300

    Exercicios

--- Aula01 - Introdução à ciência de dados e pandas/aula1.py
+++ Aula01 - Introdução à ciência de dados e pandas/aula1.py
@@ -69,3 +69,44 @@ print(string1)
 
 string1 = string1.lower()
 print(string1)
+
+
+
+
+#Exercícios
+
+# 1. Dada a lista de nomes, crie uma nova lista com apenas os nomes com mais de 5 letras
+nomes = ["Ana", "Bruno", "Carla", "Daniela", "Eva", "Fernanda", "Igor"]
+# Resultado esperado: ["Bruno", "Carla", "Daniela", "Fernanda"]
+
+# 2. Dada a lista de notas de alunos, calcule a média apenas dos alunos que tiraram nota maior ou igual a 7
+notas = [5.5, 8.2, 6.0, 7.0, 9.5, 3.0, 7.5]
+# Resultado esperado: média das notas maiores ou iguais a 7
+
+# 3. Escreva uma função que recebe uma lista de notas e retorna:
+# a média e o conceito (A: >= 9, B: >= 7, C: >= 5, D: <5)
+def analisar_notas(lista_notas):
+    pass
+# Exemplo:
+# analisar_notas([7, 8, 9]) → (8.0, "B")
+
+# 4. Dada a lista de notas de alunos, calcule a média apenas dos alunos que tiraram nota maior ou igual a 7
+notas = [5.5, 8.2, 6.0, 7.0, 9.5, 3.0, 7.5]
+# Resultado esperado: média das notas maiores ou iguais a 7
+
+# 5. Dada uma lista de dicionários com informações de pessoas, calcule a média das idades.
+
+dados = [
+    {"nome": "Ana", "idade": 23},
+    {"nome": "Bruno", "idade": 25},
+    {"nome": "Carlos", "idade": 30},
+    {"nome": "Diana", "idade": 20}
+]
+# Resultado esperado: média das idades (24.5)
+# Exemplo:
+# analisar_notas([7, 8, 9]) → (8.0, "B")
+
+# 6. A partir da mesma lista acima, filtre apenas as pessoas com idade acima de 24.
+
+
+
