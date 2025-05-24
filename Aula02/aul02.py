d368a9219ab8a799bb5b6a89922b7e10a429eef3
dionisio300 <dionisioassis80@gmail.com>
2025-05-24 09:09:51 -0300

    Novos arquivos

---
+++ Aula02/aul02.py
@@ -0,0 +1,7 @@
+
+
+# 1. Criando uma Series a partir de uma lista
+# 2. Definindo um índice personalizado
+# 3. Criando a partir de um dicionário
+# 4. Criando a partir de um valor escalar (repetido)
+# 5. Usando o parâmetro dtypee name
