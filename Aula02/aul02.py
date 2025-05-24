f5eb7dffc9d54a9396403f1b797498a2e2d76685
dionisio300 <dionisioassis80@gmail.com>
2025-05-24 12:00:43 -0300

    Aula 2

--- Aula02/aul02.py
+++ Aula02/aul02.py
@@ -77,7 +77,87 @@ print(listaNumeros)
 ################################################################
 ######################## NumPy #################################
 ################################################################
+import numpy as np
 
+lista1 = [1,2,3,4,5]
+lista1Dobro = []
+
+for numero in lista1:
+    dobro = numero * 2
+    lista1Dobro.append(dobro)
+
+print(lista1Dobro)
+
+lista2 = np.array(lista1)
+print(lista2)
+
+lista2 = lista2 + 2
+
+print(lista2)
+
+mat1 = [[ [1,2],[3,4] ],[ [5,6],[7,8] ]] 
+print(mat1)
+
+mat2 = np.array(mat1)
+print(mat2)
+#print(mat2)
+
+vetor = [1,2,3,4,5]
+mat1 = [[1,2,3],[4,5,6],[7,8,9]]
+cubo = [[ [1,2],[3,4] ],[ [5,6],[7,8] ]] 
+
+vetor = np.array(vetor)
+mat1 = np.array(mat1)
+cubo = np.array(cubo)
+
+# Função para saber o número de dimenções
+print(vetor.ndim)
+print(mat1.ndim)
+print(cubo.ndim)
+
+print(vetor.shape)
+print(mat1.shape)
+print(cubo.shape)
+
+print(vetor.size)
+print(mat1.size)
+print(cubo.size)
+
+matZero = np.zeros((3,3))
+print(matZero + 10)
+
+matUm = np.ones((2,2))
+print(matUm)
+
+matID = np.eye(3)
+print(matID)
+
+
+vetor1 = np.arange(0,1.05,0.05)
+print(vetor1)
+
+matAleatorio = np.random.rand(10, 10)
+print(np.floor(matAleatorio * 101))
+
+# 1. Crie:
+# 	Um array 1D de 5 inteiros.
+# 	Uma matriz 3x3 de números aleatórios inteiros entre 0 e 10.
+
+# 2. Faça:
+# 	A soma de dois arrays.
+# 	A multiplicação de um array por um número.
+# 	O quadrado de todos os elementos de um array.
+# 3. Mostre:
+
+# 	O número de dimensões (ndim), forma (shape), tipo (dtype) e 	a quantidade de elementos (size) de um array 2D.
+
+
+# Estatística:
+# amostras e população
+# média, mediana, moda
+# Variância, desvio padrão, desvio médio
+# correlação, autocorrelação
+# Matrizes
 
 
 
