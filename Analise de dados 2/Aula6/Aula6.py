13962caf1ec1902198b6322e5c3a297c5577a5c6
dionisio300 <dionisioassis80@gmail.com>
2025-08-16 08:37:52 -0300

    Aula 06

---
+++ Analise de dados 2/Aula6/Aula6.py
@@ -0,0 +1,23 @@
+# Carregar os dados do census.csv
+
+# Label Encoder
+
+# OneHotEncoder
+
+# Normalização
+
+# Aplicar os Algoritmos estudados até agora
+
+# Naive Bayes
+# Árvore de decisão
+# Randon Forest
+# KNN
+# Regressão Logísitica
+# SVM
+# Redes Neurais
+
+# Printar as acurácias de todos 
+# Naive Bayes = x%
+# Árvore de decisão = y%
+# ...
+
